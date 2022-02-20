858bc5ed1ea435d81ab73aea24853c244431e181
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2022-02-20 11:28:59 -0700

    Redo your template to actually work

--- PTKRewards/Properties/AssemblyInfo.cs
+++ PTKRewards/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle(PKTVendorRewards.PKTVendorRewards.ModName)]
+[assembly: AssemblyTitle("PTKRewards")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany(PKTVendorRewards.PKTVendorRewards.Author)]
-[assembly: AssemblyProduct(PKTVendorRewards.PKTVendorRewards.ModName)]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PTKRewards")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4358610B-F3F4-4843-B7AF-98B7BC60DCDE")]
+[assembly: Guid("F9B815D6-6EF4-44DB-9A0F-436849B24670")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion(PKTVendorRewards.PKTVendorRewards.ModVersion)]
-[assembly: AssemblyFileVersion(PKTVendorRewards.PKTVendorRewards.ModVersion)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
